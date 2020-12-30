7e3d928d6973169edd1b3b820d9f8032baf900a5
Yunyung <yungyung7654321@gmail.com>
2020-12-30 17:12:46 +0800

    Add all file needed for training and testing on Kaggle

---
+++ parser_data.py
@@ -0,0 +1,53 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import os
+from tqdm.auto import tqdm
+import shutil as sh
+
+ddf = pd.read_csv('global-wheat-detection/train.csv')
+bboxs = np.stack(df['bbox'].apply(lambda x: np.fromstring(x[1:-1], sep=',')))
+for i, column in enumerate(['x', 'y', 'w', 'h']):
+    df[column] = bboxs[:,i]
+df.drop(columns=['bbox'], inplace=True)
+df['x_center'] = df['x'] + df['w']/2
+df['y_center'] = df['y'] + df['h']/2
+df['classes'] = 0
+df = df[['image_id','x', 'y', 'w', 'h','x_center','y_center','classes']]
+df['area'] = df['w'] * df['h']
+
+df.head()
+
+df_clean = df[df['area']>300]
+df_clean = df_clean[df['w']>10]
+df_clean = df_clean[df['h']>10]
+df_clean = df_clean.drop([173,2169,118211,52868,117344,3687,2159,121633,113947])
+
+df_clean.head()
+
+print("remove {} boxes".format(df.shape[0] - df_clean.shape[0]))
+
+
+index = list(sorted(df_clean.image_id))
+
+source = 'train'
+if True:
+    for fold in [0]:
+        val_index = index[len(index)*fold//5:len(index)*(fold+1)//5]
+        for name,mini in tqdm(df_clean.groupby('image_id')):
+            if name in val_index:
+                path2save = 'val2017/'
+            else:
+                path2save = 'train2017/'
+            if not os.path.exists('convertor/fold{}/labels/'.format(fold)+path2save):
+                os.makedirs('convertor/fold{}/labels/'.format(fold)+path2save)
+            with open('convertor/fold{}/labels/'.format(fold)+path2save+name+".txt", 'w+') as f:
+                row = mini[['classes','x_center','y_center','w','h']].astype(float).values
+                row = row/1024
+                row = row.astype(str)
+                for j in range(len(row)):
+                    text = ' '.join(row[j])
+                    f.write(text)
+                    f.write("\n")
+            if not os.path.exists('convertor/fold{}/images/{}'.format(fold,path2save)):
+                os.makedirs('convertor/fold{}/images/{}'.format(fold,path2save))
+            sh.copy("global-wheat-detection/{}/{}.jpg".format(source,name),'convertor/fold{}/images/{}/{}.jpg'.format(fold,path2save,name))
